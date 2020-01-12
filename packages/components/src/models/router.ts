f573b3de9a3dc1338460da4b3723687ac70e762f
Matheus Albuquerque <land@ythecombinator.space>
2020-01-12 01:23:56 +0100

    refactor(packages/components/src/models/router): make params optional

--- packages/components/src/models/router.ts
+++ packages/components/src/models/router.ts
@@ -6,7 +6,7 @@ export type Navigate = <T>(key: string, params?: NavigationParams<T>) => {};
 
 export interface Route {
   path: string;
-  params: NavigationParams<any>;
+  params?: NavigationParams<any>;
 }
 
 export interface RouteConfig extends Pick<Route, "path"> {
