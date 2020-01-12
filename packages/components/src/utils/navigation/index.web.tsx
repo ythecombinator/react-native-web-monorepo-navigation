895ac857408372aeb9f5694e71fe9b7505da5bee
Matheus Albuquerque <land@ythecombinator.space>
2020-01-12 01:22:29 +0100

    feat(packages/components/src/utils/navigation): use url params on navigate hoook

--- packages/components/src/utils/navigation/index.web.tsx
+++ packages/components/src/utils/navigation/index.web.tsx
@@ -1,12 +1,40 @@
+import { useCallback } from 'react'
+
+import { stringify } from 'query-string'
 import { useHistory, useLocation, useParams } from 'react-router-dom'
 
-import { Route } from '../../models/router'
+import { NavigationParams, Route } from '../../models/router'
+
+// Helper functions
+
+const buildUrl = (pathname: string, params: NavigationParams<any>) => {
+  const base = window.location.href;
+  const url = new URL(pathname, base);
+
+  const searchParams = new URLSearchParams(params);
+  url.search = searchParams.toString();
+  const newUrl = url.toString();
+
+  const finalUrl = newUrl.replace(url.origin, "");
+
+  return finalUrl;
+};
+
+// Hooks
 
 const useNavigation = () => {
   const { push, goBack } = useHistory();
 
+  const navigate = useCallback(
+    (to: Route["path"], qsParams: Route["params"] = {}) => {
+      const path = buildUrl(to, qsParams);
+      push(path);
+    },
+    []
+  );
+
   return {
-    navigate: push,
+    navigate,
     goBack
   };
 };
